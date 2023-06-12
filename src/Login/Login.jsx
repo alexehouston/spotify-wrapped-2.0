67d5b04bbecbaa598dbd05d6f1365d46d04fa864
ahouston <alexehouston@gmail.com>
2023-06-12 17:09:09 -0500

    update img links

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -3,7 +3,7 @@ import "./Login.css";
 export default function Login() {
   const authEndpoint = "https://accounts.spotify.com/authorize";
   const clientId = "7ea98409438c424fb407d905d5cf18f6";
-  const redirectUri = "http://localhost:5173/";
+  const redirectUri = "https://spotify-wrapped.netlify.app/";
   const scopes = ["user-top-read"];
   const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
     "%20"
@@ -15,7 +15,7 @@ export default function Login() {
         Sign In With{" "}
         <img
           className="spotify-logo"
-          src="../../public/assets/Spotify_Logo_RGB_White.png"
+          src="assets/Spotify_Logo_RGB_White.png"
         />
       </a>
     </div>
